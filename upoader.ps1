69eab3804c71925279a0c7ee62338636c2118eb0
Dr. Christian Geuer-Pollmann <chgeuer@microsoft.com>
2014-07-17 12:56:40 +0200

    a

--- upoader.ps1
+++ upoader.ps1
@@ -1,11 +1,20 @@
 
-$jsonAssembly = [System.Reflection.Assembly]::LoadFrom("C:\Users\chgeuer\github\chgeuer\AzureLargeFileUploader\packages\Newtonsoft.Json.6.0.1\lib\net45\Newtonsoft.Json.dll")
-$storageAssembly = [System.Reflection.Assembly]::LoadFrom("C:\Users\chgeuer\github\chgeuer\AzureLargeFileUploader\packages\WindowsAzure.Storage.3.0.2.0\lib\net40\Microsoft.WindowsAzure.Storage.dll")
-
+$rootfolder = "C:\Users\chgeuer\github\chgeuer\AzureLargeFileUploader"
+$jsonAssembly = [System.Reflection.Assembly]::LoadFrom("$rootfolder\packages\Newtonsoft.Json.6.0.1\lib\net45\Newtonsoft.Json.dll")
+$storageAssembly = [System.Reflection.Assembly]::LoadFrom("$rootfolder\packages\WindowsAzure.Storage.3.0.2.0\lib\net40\Microsoft.WindowsAzure.Storage.dll")
 $cscode = ((New-Object -TypeName System.Net.WebClient).DownloadString("https://github.com/chgeuer/AzureLargeFileUploader/raw/master/LargeFileUploaderUtils.cs"))
-
 Add-Type -TypeDefinition $cscode -ReferencedAssemblies $jsonAssembly.Location,$storageAssembly.Location
 
-[LargeFileUploader.LargeFileUploaderUtils]::Log = [System.Console]::WriteLine
-[LargeFileUploader.LargeFileUploaderUtils]::UploadAsync("C:\Users\chgeuer\format504015.mp4", "DefaultEndpointsProtocol=https;AccountName=chgeuerams2;AccountKey=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX==", "dummy1", 2).Wait()
-Write-Host "Done"
+
+[LargeFileUploader.LargeFileUploaderUtils]::UseConsoleForLogging()
+[LargeFileUploader.LargeFileUploaderUtils]::NumBytesPerChunk = 1024
+
+$containername = "dummyps1"
+$storageaccount = "DefaultEndpointsProtocol=https;AccountName=chgeuerams2;AccountKey=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX=="
+
+Write-Host "Start upload"
+$task = [LargeFileUploader.LargeFileUploaderUtils]::UploadAsync("C:\Users\chgeuer\github\chgeuer\AzureLargeFileUploader\LargeFileUploader.csproj", $storageaccount, $containername, 2)
+Write-Host "Upload started"
+$task.Wait()
+Write-Host "Upload finished"
+
