7bd8fd716f2a18f9f993e61f38664bcbfc4b802d
Dr. Christian Geuer-Pollmann <chgeuer@microsoft.com>
2014-07-17 13:38:19 +0200

    a

--- upoader.ps1
+++ upoader.ps1
@@ -1,7 +1,7 @@
 
 $rootfolder = "C:\Users\chgeuer\github\chgeuer\AzureLargeFileUploader"
-$jsonAssembly = [System.Reflection.Assembly]::LoadFrom("$rootfolder\packages\Newtonsoft.Json.6.0.1\lib\net45\Newtonsoft.Json.dll")
-$storageAssembly = [System.Reflection.Assembly]::LoadFrom("$rootfolder\packages\WindowsAzure.Storage.3.0.2.0\lib\net40\Microsoft.WindowsAzure.Storage.dll")
+$jsonAssembly = [System.Reflection.Assembly]::LoadFrom("$rootfolder\packages\Newtonsoft.Json.6.0.3\lib\net45\Newtonsoft.Json.dll")
+$storageAssembly = [System.Reflection.Assembly]::LoadFrom("$rootfolder\packages\WindowsAzure.Storage.4.1.0\lib\net40\Microsoft.WindowsAzure.Storage.dll")
 $cscode = ((New-Object -TypeName System.Net.WebClient).DownloadString("https://github.com/chgeuer/AzureLargeFileUploader/raw/master/LargeFileUploaderUtils.cs"))
 Add-Type -TypeDefinition $cscode -ReferencedAssemblies $jsonAssembly.Location,$storageAssembly.Location
 
@@ -9,8 +9,8 @@ Add-Type -TypeDefinition $cscode -ReferencedAssemblies $jsonAssembly.Location,$s
 [LargeFileUploader.LargeFileUploaderUtils]::UseConsoleForLogging()
 [LargeFileUploader.LargeFileUploaderUtils]::NumBytesPerChunk = 1024
 
+$storageaccount = [System.Environment]::GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING")
 $containername = "dummyps1"
-$storageaccount = "DefaultEndpointsProtocol=https;AccountName=chgeuerams2;AccountKey=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX=="
 
 Write-Host "Start upload"
 $task = [LargeFileUploader.LargeFileUploaderUtils]::UploadAsync("C:\Users\chgeuer\github\chgeuer\AzureLargeFileUploader\LargeFileUploader.csproj", $storageaccount, $containername, 2)
